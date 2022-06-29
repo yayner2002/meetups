940d86dd40cef6a14acee40a3bde243eaddc9f82
yayner <yayner2002@gmail.com>
2022-06-29 15:20:43 +0300

    useEffect to load data conditionally

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,26 +1,37 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
-  fetch("https://reactmeetups-3b740-default-rtdb.firebaseio.com/meetups.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://reactmeetups-3b740-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
-    if(isLoading){
-      return (
-        <section>
-          <p>Loading...</p>
-        </section>
-      )
-    }
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <div>
       <section>
